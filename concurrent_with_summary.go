d41500f8298ae2416ce28764394886ea5f74a39f
Irakli N <inadarei@users.noreply.github.com>
2017-12-13 01:40:14 -0500

    init

--- concurrent_with_summary.go
+++ concurrent_with_summary.go
@@ -4,23 +4,37 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"sync"
 
 	"github.com/buger/jsonparser"
 )
 
+const baseURL = "https://www.googleapis.com/books/v1/volumes?q="
+
 func main() {
-	var authors []string
+	//var authors []string
 	const isbn = "1491956224" // "Microservice Architecture"
-	authors = bookAuthors(isbn)
+	authors := bookAuthors(isbn)
 
-	fmt.Printf("%v", authors)
+	var wg sync.WaitGroup
+	wg.Add(len(authors))
 
+	for _, author := range authors {
+		go func(author string) {
+			defer wg.Done()
+
+			numBooks, authorName := authorNumBooks(author)
+			fmt.Printf("%s authored %d books \n", authorName, numBooks)
+		}(author)
+	}
+
+	wg.Wait()
 }
 
 /* bookAuthors returns all authors of a book */
 func bookAuthors(isbn string) []string {
-	baseURL := "https://www.googleapis.com/books/v1/volumes?q=isbn:"
-	bookURL := baseURL + isbn
+	bookURL := baseURL + "isbn:" + isbn
 
 	res, err := http.Get(bookURL)
 	if err != nil {
@@ -43,7 +57,25 @@ func bookAuthors(isbn string) []string {
 	return authors
 }
 
-/* authorBooks returns number of books an author has published */
-func authorNumBooks() {
+/* authorBooks returns books an author has published */
+func authorNumBooks(authorName string) (int, string) {
+	authorNameSafe := url.QueryEscape(authorName)
+	apiURL := baseURL + "inauthor:\"" + authorNameSafe + "\""
 
+	res, err := http.Get(apiURL)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	numBooks, err := jsonparser.GetInt(body, "totalItems")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return int(numBooks), authorName
 }
